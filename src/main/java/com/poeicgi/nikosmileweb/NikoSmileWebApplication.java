2f06d6f14def4be4578f487f9020c5a413937e41
feduc <felix.ducoinnezan@gmail.com>
2017-03-13 11:16:37 +0100

    premier git

---
+++ src/main/java/com/poeicgi/nikosmileweb/NikoSmileWebApplication.java
@@ -0,0 +1,12 @@
+package com.poeicgi.nikosmileweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NikoSmileWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NikoSmileWebApplication.class, args);
+	}
+}
